1658879a7b9279897d69622f5ebf3df8166b7792
马春静 <suda_machunjing@163.com>
2017-04-10 15:22:10 +0800

    iOS端适配0.40

--- android/app/src/main/java/com/reemii/tutu/driver/MainApplication.java
+++ android/app/src/main/java/com/reemii/tutu/driver/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNSpinkitPackage(),
             new RCTCameraPackage(),
